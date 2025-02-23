efa5cb810730db72cb156d476b6970c1742790e0
Nathan Kitchen <nathan.kitchen@gmail.com>
2025-02-22 20:48:57 -0800

    Day 24, part 2

--- src/advent/year_2024/day_24/core.clj
+++ src/advent/year_2024/day_24/core.clj
@@ -1,7 +1,9 @@
 (ns advent.year-2024.day-24.core
   (:require
    [advent.tools :as tools]
-   [clojure.string :as str]))
+   [clojure.string :as str]
+   [clojure.pprint :as pp]
+   [clojure.set :as set]))
 
 (defn read-input [file]
   (let [[known-lines rule-lines] (tools/read-blocks file)
@@ -50,33 +52,56 @@
 
 (defn part-2 [file]
   (let [[initially-known rules] (read-input file)
-        zero-inputs (->> initially-known
-                         (map (fn [[input _]]
-                                [input 0]))
-                         (into {}))]
-    (->> (range)
-         (map (fn [i]
-                (let [output (wire-name "z" i)
-                      input-1 (wire-name "x" i)
-                      input-2 (wire-name "y" i)
-                      check (fn [x y z]
-                              (= z ((evaluator (assoc zero-inputs
-                                                      input-1 x
-                                                      input-2 y)
-                                               rules)
-                                    output)))]
-                  (when (rules output)
-                    [output (and
-                             (check 0 0 0)
-                             (check 0 1 1)
-                             (check 1 0 1)
-                             (check 1 1 0))]))))
-         (take-while some?))))
+        imax (apply max (->> (range)
+                               (take-while #(initially-known (wire-name "x" %)))))
+        n (inc imax)
+        gate-of (into {} (for [[output [a op b]] rules]
+                           [output [op #{a b}]]))
+        gate2-out (set/map-invert gate-of)
+        gate1-out (into {} (for [[output [op inputs]] gate-of
+                                 in inputs]
+                             [[op in] output]))]
+    (loop [i 1
+           cin "drq" ; carry in
+           ]
+      (let [[sigs miswired]
+            (reduce
+              (fn [[sigs miswired] [out op in1 in2]]
+                (let [in1-sig (sigs in1)
+                      in2-sig (sigs in2)
+                      out-sig (gate2-out [op #{in1-sig in2-sig}])
+                      out-sig1 (gate1-out [op in1-sig])
+                      out-sig2 (gate1-out [op in2-sig])]
+                  (cond
+                    out-sig [(assoc sigs out out-sig) miswired]
+                    out-sig1 [(assoc sigs out out-sig1) (conj miswired [in2-sig :not op in1-sig in2-sig])]
+                    out-sig2 [(assoc sigs out out-sig2) (conj miswired [in1-sig :not op in1-sig in2-sig])]
+                    :else (assert false [i sigs]))))
 
-(comment
+              [{:x (wire-name "x" i)
+                :y (wire-name "y" i)
+                :cin cin}
+               #{}]
+
+              [[:s1 "XOR" :x :y]
+               [:cg "AND" :x :y]
+               [:s2 "XOR" :s1 :cin]
+               [:cp "AND" :cin :s1]
+               [:co "OR" :cg :cp]])]
+
+        (if (seq miswired)
+          (println "Miswired: " miswired))
+        (if (< i 44)
+          (recur (inc i)
+                 (sigs :co)))))))
+
+; (comment
+(doseq [r [
   (format "%02d" 3)
-  (part-1 "test1")
-  (part-1 "test2")
-  (part-1 "input")
-  (part-2 "test3")
-  (part-2 "input"))
+  (time (part-1 "test1"))
+  (time (part-1 "test2"))
+  (time (part-1 "input"))
+  ;(time (part-2 "test3"))
+  (time (part-2 "input"))
+  ]]
+  (println r))
